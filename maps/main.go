0940eda3012f77935a18e4fe5f34b7a4ba796ff2
harsh91 <harsh_fresno@mail.fresnostate.edu>
2016-02-08 13:23:07 -0800

    Getting hands on maps in golang.

---
+++ maps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	scoreMap := map[string]string {
+		"Harsh": "100",
+		"Abhas": "90",
+		"Deepti": "80",
+		"Sarika": "95",
+		"Jeev": "91",
+	}
+	fmt.Println("Maps in intially looks like")
+	fmt.Println(scoreMap)
+	scoreMap["Mukasir"] = "70"
+	fmt.Println("Maps after adding an element looks like")
+	fmt.Println(scoreMap)
+	fmt.Print("Deleting : ")
+	fmt.Println(scoreMap["Mukasir"])
+	delete(scoreMap, "Mukasir")
+	fmt.Println("After deleting map looks like")
+	fmt.Println(scoreMap)
+
+	for keys, values := range scoreMap {
+		fmt.Println(keys + ": " + values)
+	}
+}
