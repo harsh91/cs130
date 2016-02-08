59e570ebd6d83f148b95307b178d03877001692c
harsh91 <harsh_fresno@mail.fresnostate.edu>
2016-02-08 13:16:41 -0800

    Adding a new handlefunc there for new url.

--- surfer_page/main.go
+++ surfer_page/main.go
@@ -14,8 +14,17 @@ func serveRoot(res http.ResponseWriter, req *http.Request) {
 	tpl.Execute(res, nil)
 }
 
+func serveRootDeepti(res http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles("indexdeepti.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.Execute(res, nil)
+}
+
 func main() {
 	http.Handle("/img/", http.StripPrefix("/img", http.FileServer(http.Dir("img"))))
 	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/deepti/", serveRootDeepti)
 	http.ListenAndServe(":8080", nil)
 }
